35901ae07b64e9b9972663f935f90386ae5c1d8e
Aleck ASTAN <aleck.dev.git@gmail.com>
2021-10-11 17:58:07 -0700

    Fixtures installed

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
