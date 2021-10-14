d7ab80fad9d1d66636eaaaf03bea25d1f7288848
Aleck ASTAN <aleck.dev.git@gmail.com>
2021-10-13 23:38:34 -0700

    Crud + image optmisation Done

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -65,7 +65,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        return new RedirectResponse($this->urlGenerator->generate('home'));
         throw new \Exception(
             'TODO: provide a valid redirect inside ' . __FILE__
         );
